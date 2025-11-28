fc3e45a09f34119fa7e252c5a93a79e43117fc86
kaa08 <kaa08@naver.com>
2025-11-28 15:41:33 +0900

    feature: post like first implement

--- src/main/java/com/example/community/dto/response/post/PostLikeResponse.java
+++ src/main/java/com/example/community/dto/response/post/PostLikeResponse.java
@@ -1,4 +1,28 @@
 package com.example.community.dto.response.post;
 
+import com.example.community.domain.PostLike;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 public class PostLikeResponse {
+
+    private Long postId;
+    private Boolean liked;
+    private Long likeCount;
+
+    @Builder
+    public PostLikeResponse(Long postId, boolean liked, long likeCount) {
+        this.postId = postId;
+        this.liked = liked;
+        this.likeCount = likeCount;
+    }
+
+    public static PostLikeResponse fromEntity(PostLike postLike, Boolean liked, long likeCount) {
+        return PostLikeResponse.builder()
+                .postId(postLike.getPost().getId())
+                .liked(liked)
+                .likeCount(likeCount)
+                .build();
+    }
 }
