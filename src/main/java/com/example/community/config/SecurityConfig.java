575e37420dca5bbc999bc2722134968837ae9219
kaa08 <kaa08@naver.com>
2025-11-28 16:47:46 +0900

    fix Authentication Filter Allowed Method update

--- src/main/java/com/example/community/config/SecurityConfig.java
+++ src/main/java/com/example/community/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -56,13 +57,14 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/webjars/**"
                         ).permitAll()
-
                         .requestMatchers(
                                 "/auth/**",
                                 "/users/**",
-                                "/posts",
-                                "/posts/**",
                                 "/presigned-url"
+                        ).permitAll()
+                        .requestMatchers(HttpMethod.GET,
+                                "/posts",
+                                "/posts/**"
                         ).permitAll()
 
                         .anyRequest().authenticated()
